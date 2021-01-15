0a98b0e697a9e606d987fd37b0cd4a79dd6df63d
bogdan.gutu <bogygbm@gmail.com>
2021-01-16 01:22:14 +0200

    Test Mockito
    Am adaugat un test de tip Mockito
    
    Signed-off-by: bogdan.gutu <bogygbm@gmail.com>

---
+++ src/test/java/Model/WeatherAppModelTestMockito.java
@@ -0,0 +1,53 @@
+package Model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.util.Scanner;
+
+import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
+
+public class WeatherAppModelTestMockito {
+    String cityName;
+    String countryName;
+    String countryCode;
+
+    private WeatherAppModel testModelMockito;
+    @Before
+    public void setUp() throws Exception {
+        BufferedReader inputStreamReader=mock(BufferedReader.class);
+        when(inputStreamReader.readLine()).thenReturn("Gaborone").thenReturn("Botswana").thenReturn("BW");
+
+        testModelMockito=new WeatherAppModel(inputStreamReader.readLine(),inputStreamReader.readLine(),inputStreamReader.readLine());
+
+        File myObj = new File("src/main/resources/input.txt");
+        Scanner myReader = new Scanner(myObj);
+        String data = myReader.nextLine();
+        String delims = "#";
+        String[] tokens = data.split(delims);
+        cityName=tokens[1];
+        countryName=tokens[2];
+        countryCode=tokens[0];
+    }
+
+    @Test
+    public void getCityName() {
+        assertEquals(testModelMockito.getCityName(),cityName);
+    }
+
+    @Test
+    public void getCountryName() {
+        assertEquals(testModelMockito.getCountryName(),countryName);
+    }
+
+    @Test
+    public void getCountryCode() {
+        assertEquals(testModelMockito.getCountryCode(),countryCode);
+    }
+}
