0b734bb725b20be2e5683539a466ab391cb04fe9
bogdan.gutu <bogygbm@gmail.com>
2021-01-16 01:01:23 +0200

    Test Model
    Am adaugat un test pentru Clasa WeatherAppModel
    
    Signed-off-by: bogdan.gutu <bogygbm@gmail.com>

---
+++ src/test/java/Model/WeatherAppModelTest.java
@@ -0,0 +1,43 @@
+package Model;
+
+import java.io.File;
+import java.util.Scanner;
+
+import static org.junit.Assert.*;
+import static junit.framework.TestCase.assertEquals;
+public class WeatherAppModelTest {
+
+    String cityName;
+    String countryName;
+    String countryCode;
+    WeatherAppModel model;
+    @org.junit.Before
+    public void setUp() throws Exception {
+        cityName="Gaborone";
+        countryCode="BW";
+        countryName="Botswana";
+        File myObj = new File("src/main/resources/input.txt");
+        Scanner myReader = new Scanner(myObj);
+        String data = myReader.nextLine();
+        String delims = "#";
+        String[] tokens = data.split(delims);
+        model= new WeatherAppModel(tokens[1], tokens[2], tokens[0]);
+
+    }
+
+    @org.junit.Test
+    public void getCityName() {
+        assertEquals(model.getCityName(),cityName);
+    }
+
+    @org.junit.Test
+    public void getCountryName() {
+        assertEquals(model.getCountryName(),countryName);
+    }
+
+    @org.junit.Test
+    public void getCountryCode() {
+        assertEquals(model.getCountryCode(),countryCode);
+    }
+
+}
